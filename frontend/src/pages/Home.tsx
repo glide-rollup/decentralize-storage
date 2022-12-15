f7a02773283841b08c16a5b1b97e8dd659ac3a5e
Vlodkow <vlodkow@gmail.com>
2022-12-15 12:34:25 +0200

    Update homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,9 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import filecoinLogo from '../assets/img/filecoin.png';
 import nftStorageLogo from '../assets/img/nft-storage.svg';
+import ownIcon from '../assets/img/own.png';
+import versionIcon from '../assets/img/versions.png';
+import dirsIcon from '../assets/img/dirs.png';
 import {Wrapper} from "../assets/css/common.style";
 
 export default function Home() {
@@ -135,54 +138,12 @@ export default function Home() {
                   </g>
                 </svg>
 
-                <svg className="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-1TriangleIcon1">
-                      <stop stopColor="#9C09DB" offset="0%"/>
-                      <stop stopColor="#1C0FD7" offset="100%"/>
-                    </linearGradient>
-                    <filter x="-14%" y="-10%" width="128%" height="128%" filterUnits="objectBoundingBox"
-                            id="filter-3TriangleIcon1">
-                      <feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1"/>
-                      <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1"
-                                      result="shadowBlurOuter1"/>
-                      <feColorMatrix
-                        values="0 0 0 0 0.141176471 0 0 0 0 0.031372549 0 0 0 0 0.501960784 0 0 0 0.15 0"
-                        in="shadowBlurOuter1"/>
-                    </filter>
-                    <path
-                      d="M17.947 0h14.106c6.24 0 8.503.65 10.785 1.87a12.721 12.721 0 015.292 5.292C49.35 9.444 50 11.707 50 17.947v14.106c0 6.24-.65 8.503-1.87 10.785a12.721 12.721 0 01-5.292 5.292C40.556 49.35 38.293 50 32.053 50H17.947c-6.24 0-8.503-.65-10.785-1.87a12.721 12.721 0 01-5.292-5.292C.65 40.556 0 38.293 0 32.053V17.947c0-6.24.65-8.503 1.87-10.785A12.721 12.721 0 017.162 1.87C9.444.65 11.707 0 17.947 0z"
-                      id="path-2TriangleIcon1"/>
-                  </defs>
-                  <g id="Page-1TriangleIcon1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <g id="Desktop-HDTriangleIcon1" transform="translate(-291 -1278)">
-                      <g id="FeaturesTriangleIcon1" transform="translate(170 915)">
-                        <g id="Group-9TriangleIcon1" transform="translate(0 365)">
-                          <g id="Group-8TriangleIcon1" transform="translate(125)">
-                            <g id="Rectangle-9TriangleIcon1">
-                              <use fill="#000" filter="url(#filter-3TriangleIcon1)"
-                                   xlinkHref="#path-2TriangleIcon1"/>
-                              <use fill="url(#linearGradient-1TriangleIcon1)"
-                                   xlinkHref="#path-2TriangleIcon1"/>
-                            </g>
-                            <g id="playTriangleIcon1" transform="translate(18 15)" fill="#FFF"
-                               fillRule="nonzero">
-                              <path
-                                d="M9.432 2.023l8.919 14.879a1.05 1.05 0 01-.384 1.452 1.097 1.097 0 01-.548.146H-.42A1.07 1.07 0 01-1.5 17.44c0-.19.052-.375.15-.538L7.567 2.023a1.092 1.092 0 011.864 0z"
-                                id="TriangleIcon1" transform="rotate(90 8.5 10)"/>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-                <h4 className="relative mt-6 text-lg font-bold">Automated Tools</h4>
-                <p className="relative mt-2 text-base text-center text-gray-600">Automate your workflow with these
-                  top of the
-                  line marketing tools.</p>
-                <a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn
-                  More</a>
+                <img src={versionIcon} className="relative w-28 h-28" alt=""/>
+                <h4 className="relative mt-2 text-lg font-bold">File Versioning</h4>
+                <p className="relative mt-2 text-base text-center text-gray-600">
+                  Store multiple file versions with simple and powerful user interface with versioning history.
+                </p>
+                {/*<a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn More</a>*/}
               </div>
             </div>
 
@@ -198,53 +159,12 @@ export default function Home() {
                   </g>
                 </svg>
 
-                <svg className="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-1Icon2">
-                      <stop stopColor="#F2C314" offset="0%"/>
-                      <stop stopColor="#FC3832" offset="100%"/>
-                    </linearGradient>
-                    <filter x="-14%" y="-10%" width="128%" height="128%" filterUnits="objectBoundingBox"
-                            id="filter-3Icon2">
-                      <feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1"/>
-                      <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1"
-                                      result="shadowBlurOuter1"/>
-                      <feColorMatrix
-                        values="0 0 0 0 0.501960784 0 0 0 0 0.125490196 0 0 0 0 0 0 0 0 0.15 0"
-                        in="shadowBlurOuter1"/>
-                    </filter>
-                    <path
-                      d="M17.947 0h14.106c6.24 0 8.503.65 10.785 1.87a12.721 12.721 0 015.292 5.292C49.35 9.444 50 11.707 50 17.947v14.106c0 6.24-.65 8.503-1.87 10.785a12.721 12.721 0 01-5.292 5.292C40.556 49.35 38.293 50 32.053 50H17.947c-6.24 0-8.503-.65-10.785-1.87a12.721 12.721 0 01-5.292-5.292C.65 40.556 0 38.293 0 32.053V17.947c0-6.24.65-8.503 1.87-10.785A12.721 12.721 0 017.162 1.87C9.444.65 11.707 0 17.947 0z"
-                      id="path-2Icon2"/>
-                  </defs>
-                  <g id="Page-1Icon2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <g id="Desktop-HDIcon2" transform="translate(-691 -1278)">
-                      <g id="FeaturesIcon2" transform="translate(170 915)">
-                        <g id="Group-9-CopyIcon2" transform="translate(400 365)">
-                          <g id="Group-8Icon2" transform="translate(125)">
-                            <g id="Rectangle-9Icon2">
-                              <use fill="#000" filter="url(#filter-3Icon2)"
-                                   xlinkHref="#path-2Icon2"/>
-                              <use fill="url(#linearGradient-1Icon2)" xlinkHref="#path-2Icon2"/>
-                            </g>
-                            <g id="machine-learningIcon2" transform="translate(14 12)" fill="#FFF"
-                               fillRule="nonzero">
-                              <path
-                                d="M10.554 21.418v-2.68c-1.1-.204-1.932-1.143-1.932-2.271 0-.468.143-.903.388-1.267l-2.32-1.662L4.367 15.2a2.254 2.254 0 01-.005 2.541l5.28 4.05c.268-.182.577-.311.911-.373zm.892 0c.334.062.643.191.912.373l5.28-4.05a2.254 2.254 0 01-.006-2.54l-2.321-1.663L12.99 15.2c.245.364.388.8.388 1.267 0 1.128-.832 2.067-1.932 2.27v2.681zm1.538.997c.25.365.394.803.394 1.274C13.378 24.965 12.314 26 11 26s-2.378-1.035-2.378-2.311c0-.471.145-.91.394-1.274l-5.28-4.05c-.385.26-.853.413-1.358.413C1.065 18.778 0 17.743 0 16.467c0-1.129.832-2.068 1.932-2.27v-2.393C.832 11.6 0 10.662 0 9.534c0-1.277 1.065-2.312 2.378-2.312.505 0 .973.153 1.358.414l5.28-4.05a2.254 2.254 0 01-.394-1.275C8.622 1.035 9.686 0 11 0s2.378 1.035 2.378 2.311c0 .471-.145.91-.394 1.274l5.28 4.05c.385-.26.853-.413 1.358-.413C20.935 7.222 22 8.257 22 9.533c0 1.129-.832 2.068-1.932 2.27v2.393c1.1.203 1.932 1.142 1.932 2.27 0 1.277-1.065 2.312-2.378 2.312-.505 0-.973-.153-1.358-.414l-5.28 4.05zm-9.243-7.843L5.937 13l-2.196-1.572c-.27.183-.58.314-.917.376v2.392c.336.062.647.193.917.376zm.627-3.772l2.321 1.662L9.01 10.8a2.254 2.254 0 01-.388-1.267c0-1.128.832-2.067 1.932-2.27V4.582a2.403 2.403 0 01-.912-.373l-5.28 4.05a2.254 2.254 0 01.006 2.54zm13.89 3.772c.27-.183.582-.314.918-.376v-2.392a2.403 2.403 0 01-.917-.376L16.063 13l2.196 1.572zm-.62-6.313l-5.28-4.05a2.403 2.403 0 01-.912.373v2.68c1.1.204 1.932 1.143 1.932 2.271 0 .468-.143.903-.388 1.267l2.32 1.662 2.322-1.662a2.254 2.254 0 01.005-2.541zm-8 6.313A2.415 2.415 0 0111 14.156c.507 0 .977.154 1.363.416L14.559 13l-2.196-1.572a2.415 2.415 0 01-1.363.416c-.507 0-.977-.154-1.363-.416L7.441 13l2.196 1.572zM11 10.978c.821 0 1.486-.647 1.486-1.445 0-.797-.665-1.444-1.486-1.444s-1.486.647-1.486 1.444c0 .798.665 1.445 1.486 1.445zm0 6.933c.821 0 1.486-.647 1.486-1.444 0-.798-.665-1.445-1.486-1.445s-1.486.647-1.486 1.445c0 .797.665 1.444 1.486 1.444zm8.622-6.933c.82 0 1.486-.647 1.486-1.445 0-.797-.665-1.444-1.486-1.444s-1.487.647-1.487 1.444c0 .798.666 1.445 1.487 1.445zm0 6.933c.82 0 1.486-.647 1.486-1.444 0-.798-.665-1.445-1.486-1.445s-1.487.647-1.487 1.445c0 .797.666 1.444 1.487 1.444zM2.378 10.978c.821 0 1.487-.647 1.487-1.445 0-.797-.666-1.444-1.487-1.444-.82 0-1.486.647-1.486 1.444 0 .798.665 1.445 1.486 1.445zm0 6.933c.821 0 1.487-.647 1.487-1.444 0-.798-.666-1.445-1.487-1.445-.82 0-1.486.647-1.486 1.445 0 .797.665 1.444 1.486 1.444zM11 25.133c.821 0 1.486-.646 1.486-1.444 0-.798-.665-1.445-1.486-1.445s-1.486.647-1.486 1.445.665 1.444 1.486 1.444zm0-21.377c.821 0 1.486-.647 1.486-1.445S11.821.867 11 .867s-1.486.646-1.486 1.444c0 .798.665 1.445 1.486 1.445z"
-                                id="ShapeIcon2"/>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-                <h4 className="relative mt-6 text-lg font-bold">Machine Learning</h4>
-                <p className="relative mt-2 text-base text-center text-gray-600">Your Marketing tools will learn how
-                  to better
-                  convert and sell.</p>
-                <a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn
-                  More</a>
+                <img src={ownIcon} className="relative w-24 h-24" alt=""/>
+                <h4 className="relative mt-2 text-lg font-bold">Own your Data</h4>
+                <p className="relative mt-2 text-base text-center text-gray-600">
+                  Become the real owner of your data by using IPFS you can provide API key to use your own account.
+                </p>
+                {/*<a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn More</a>*/}
               </div>
             </div>
 
@@ -260,53 +180,12 @@ export default function Home() {
                   </g>
                 </svg>
 
-                <svg className="relative w-20 h-20" viewBox="0 0 58 58" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <linearGradient x1="0%" y1="0%" x2="100%" y2="100%" id="linearGradient-1Icon3">
-                      <stop stopColor="#32FBFC" offset="0%"/>
-                      <stop stopColor="#3214F2" offset="100%"/>
-                    </linearGradient>
-                    <filter x="-14%" y="-10%" width="128%" height="128%" filterUnits="objectBoundingBox"
-                            id="filter-3Icon3">
-                      <feOffset dy="2" in="SourceAlpha" result="shadowOffsetOuter1"/>
-                      <feGaussianBlur stdDeviation="2" in="shadowOffsetOuter1"
-                                      result="shadowBlurOuter1"/>
-                      <feColorMatrix
-                        values="0 0 0 0 0.031372549 0 0 0 0 0.149019608 0 0 0 0 0.658823529 0 0 0 0.15 0"
-                        in="shadowBlurOuter1"/>
-                    </filter>
-                    <path
-                      d="M17.947 0h14.106c6.24 0 8.503.65 10.785 1.87a12.721 12.721 0 015.292 5.292C49.35 9.444 50 11.707 50 17.947v14.106c0 6.24-.65 8.503-1.87 10.785a12.721 12.721 0 01-5.292 5.292C40.556 49.35 38.293 50 32.053 50H17.947c-6.24 0-8.503-.65-10.785-1.87a12.721 12.721 0 01-5.292-5.292C.65 40.556 0 38.293 0 32.053V17.947c0-6.24.65-8.503 1.87-10.785A12.721 12.721 0 017.162 1.87C9.444.65 11.707 0 17.947 0z"
-                      id="path-2Icon3"/>
-                  </defs>
-                  <g id="Page-1Icon3" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    <g id="Desktop-HDIcon3" transform="translate(-1091 -1278)">
-                      <g id="FeaturesIcon3" transform="translate(170 915)">
-                        <g id="Group-9-Copy-2Icon3" transform="translate(800 365)">
-                          <g id="Group-8Icon3" transform="translate(125)">
-                            <g id="Rectangle-9Icon3">
-                              <use fill="#000" filter="url(#filter-3Icon3)"
-                                   xlinkHref="#path-2Icon3"/>
-                              <use fill="url(#linearGradient-1Icon3)" xlinkHref="#path-2Icon3"/>
-                            </g>
-                            <g id="smart-notificationsIcon3" transform="translate(15 11)"
-                               fill="#FFF" fillRule="nonzero">
-                              <path
-                                d="M12.519 3.243a6.808 6.808 0 00-.187 1.298h-8.44a2.595 2.595 0 00-2.595 2.594v12.973a2.595 2.595 0 002.595 2.595h12.973a2.595 2.595 0 002.594-2.595v-8.44c.445-.02.88-.084 1.298-.187v8.627A3.892 3.892 0 0116.865 24H3.892A3.892 3.892 0 010 20.108V7.135a3.892 3.892 0 013.892-3.892h8.627zm6.616 6.487a4.865 4.865 0 110-9.73 4.865 4.865 0 010 9.73z"
-                                id="IconIcon3"/>
-                            </g>
-                          </g>
-                        </g>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-                <h4 className="relative mt-6 text-lg font-bold">Smart Notifications</h4>
-                <p className="relative mt-2 text-base text-center text-gray-600">Our smart notifications will notify
-                  you when
-                  users convert.</p>
-                <a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn
-                  More</a>
+                <img src={dirsIcon} className="relative w-24 h-24" alt=""/>
+                <h4 className="relative mt-2 text-lg font-bold">Custom Structure</h4>
+                <p className="relative mt-2 text-base text-center text-gray-600">
+                  Create any directory structure for your files, control colors and manage access rights.
+                </p>
+                {/*<a href="#_" className="relative flex mt-2 text-sm font-medium text-indigo-500 underline">Learn More</a>*/}
               </div>
             </div>
 
